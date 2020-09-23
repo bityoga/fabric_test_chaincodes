def3d4b263581197a82b226c4a19674dccf0cdb2
anandhakumarpalanisamy <anandhakumarpalanisamy91@gmail.com>
2020-09-23 18:17:40 +0200

    - lifecycle script changes

--- bank_chaincode/shell_scripts_lifecycle/3_approve_chaincode_def_for_org.sh
+++ bank_chaincode/shell_scripts_lifecycle/3_approve_chaincode_def_for_org.sh
@@ -6,22 +6,22 @@ set -x #echo on
 
 CHAINCODE_NAME="bank"
 CHAINCODE_VERSION="1.0"
-PACKAGE_NAME=${CHAINCODE_NAME}.tar.gz
-CHAINCODE_LABEL=${CHAINCODE_NAME}_${CHAINCODE_VERSION}
+PACKAGE_NAME="${CHAINCODE_NAME}.tar.gz"
+CHAINCODE_LABEL="${CHAINCODE_NAME}_${CHAINCODE_VERSION}"
 CHAINCODE_SRC_CODE_PATH="/root/CLI/chaincodes/fabric_test_chaincodes/bank_chaincode/node"
 CHANCODE_LANGUAGE="node"
 
 ORGANISATION_NAME="hlfMSP"
-SIGNATURE_POLICY="OR('$ORGANISATION_NAME.peer')"
-ORDERER_CA=/root/CLI/${ORGCA_HOST}/${ORDERER_HOST}/msp/cacerts/orgca-7054.pem
-PACKAGE_ID=$(peer lifecycle chaincode queryinstalled --output json | jq .installed_chaincodes[0].package_id)
+SIGNATURE_POLICY="OR('${ORGANISATION_NAME}.member')"
 CHANNEL_NAME="appchannel"
 SEQUENCE="1"
 
-PEER_HOST=peer2
-CORE_PEER_ADDRESS=${PEER_HOST}:7051
-CORE_PEER_MSPCONFIGPATH=/root/CLI/${ORGCA_HOST}/${ADMIN_USER}/msp
-CORE_PEER_TLS_ROOTCERT_FILE=/root/CLI/${ORGCA_HOST}/${PEER_HOST}/msp/tls/ca.crt
+export ORDERER_CA=/root/CLI/${ORGCA_HOST}/${ORDERER_HOST}/msp/cacerts/orgca-7054.pem
+export PACKAGE_ID=$(peer lifecycle chaincode queryinstalled --output json | jq .installed_chaincodes[0].package_id)
+export PEER_HOST=peer2
+export CORE_PEER_ADDRESS=${PEER_HOST}:7051
+export CORE_PEER_MSPCONFIGPATH=/root/CLI/${ORGCA_HOST}/${ADMIN_USER}/msp
+export CORE_PEER_TLS_ROOTCERT_FILE=/root/CLI/${ORGCA_HOST}/${PEER_HOST}/msp/tls/ca.crt
 
 
-peer lifecycle chaincode approveformyorg -o ${ORDERER_HOST}:7050 --tls --cafile ${CORE_PEER_TLS_ROOTCERT_FILE} --channelID $CHANNEL_NAME --name $CHAINCODE_NAME --version $CHAINCODE_VERSION init-required --package-id $PACKAGE_ID --sequence $SEQUENCE --signature-policy $SIGNATURE_POLICY
+peer lifecycle chaincode approveformyorg -o ${ORDERER_HOST}:7050 --tls --cafile ${CORE_PEER_TLS_ROOTCERT_FILE} --channelID ${CHANNEL_NAME} --name ${CHAINCODE_NAME} --version ${CHAINCODE_VERSION} init-required --package-id ${PACKAGE_ID} --sequence ${SEQUENCE} --signature-policy ${SIGNATURE_POLICY}
