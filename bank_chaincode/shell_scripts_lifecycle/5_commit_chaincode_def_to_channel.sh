cbed075677d917f9bf84f7fb7e82fdc7c8f5b6df
anandhakumarpalanisamy <anandhakumarpalanisamy91@gmail.com>
2020-09-23 18:27:42 +0200

    - lifecycle script changes

--- bank_chaincode/shell_scripts_lifecycle/5_commit_chaincode_def_to_channel.sh
+++ bank_chaincode/shell_scripts_lifecycle/5_commit_chaincode_def_to_channel.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# peer lifecycle chaincode checkcommitreadiness -o ${ORDERER_HOST}:7050 --channelID appchannel --name bank --version 1.0 --tls --cafile ${ORDERER_CA} --init-required --sequence 1
 # peer lifecycle chaincode commit -o ${ORDERER_HOST}:7050 --channelID appchannel --name bank --version 1.0 --sequence 1 --init-required --tls --cafile ${ORDERER_CA} --peerAddresses ${CORE_PEER_ADDRESS} --tlsRootCertFiles ${CORE_PEER_TLS_ROOTCERT_FILE}
 
 set -x #echo on
@@ -22,7 +21,5 @@ export CORE_PEER_MSPCONFIGPATH=/root/CLI/${ORGCA_HOST}/${ADMIN_USER}/msp
 export CORE_PEER_TLS_ROOTCERT_FILE=/root/CLI/${ORGCA_HOST}/${PEER_HOST}/msp/tls/ca.crt
 export ORDERER_CA=/root/CLI/${ORGCA_HOST}/${ORDERER_HOST}/msp/cacerts/orgca-7054.pem
 export PACKAGE_ID=$(peer lifecycle chaincode queryinstalled --output json | jq .installed_chaincodes[0].package_id)
-
-peer lifecycle chaincode checkcommitreadiness -o ${ORDERER_HOST}:7050 --channelID ${CHANNEL_NAME} --name ${CHAINCODE_NAME} --version ${CHAINCODE_VERSION} --tls --cafile ${ORDERER_CA} --init-required --sequence ${SEQUENCE} &&
 
 peer lifecycle chaincode commit -o ${ORDERER_HOST}:7050 --channelID ${CHANNEL_NAME} --name ${CHAINCODE_NAME} --version ${CHAINCODE_VERSION} --sequence ${SEQUENCE} --init-required --tls --cafile ${ORDERER_CA} --peerAddresses ${CORE_PEER_ADDRESS} --tlsRootCertFiles ${CORE_PEER_TLS_ROOTCERT_FILE}
