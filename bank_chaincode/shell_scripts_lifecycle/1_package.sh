e653488e0c23b5bbc8c50e75c9553b4f0e69f076
anandhakumarpalanisamy <anandhakumarpalanisamy91@gmail.com>
2020-09-23 18:12:55 +0200

    - lifecycle script changes

--- bank_chaincode/shell_scripts_lifecycle/1_package.sh
+++ bank_chaincode/shell_scripts_lifecycle/1_package.sh
@@ -11,9 +11,9 @@ CHAINCODE_SRC_CODE_PATH="/root/CLI/chaincodes/fabric_test_chaincodes/bank_chainc
 CHANCODE_LANGUAGE="node"
 
 
-PEER_HOST=peer2
-CORE_PEER_ADDRESS=${PEER_HOST}:7051
-CORE_PEER_MSPCONFIGPATH=/root/CLI/${ORGCA_HOST}/${ADMIN_USER}/msp
-CORE_PEER_TLS_ROOTCERT_FILE=/root/CLI/${ORGCA_HOST}/${PEER_HOST}/msp/tls/ca.crt
+export PEER_HOST=peer2
+export CORE_PEER_ADDRESS=${PEER_HOST}:7051
+export CORE_PEER_MSPCONFIGPATH=/root/CLI/${ORGCA_HOST}/${ADMIN_USER}/msp
+export CORE_PEER_TLS_ROOTCERT_FILE=/root/CLI/${ORGCA_HOST}/${PEER_HOST}/msp/tls/ca.crt
 
 peer lifecycle chaincode package ${PACKAGE_NAME} --path ${CHAINCODE_SRC_CODE_PATH} --lang ${CHANCODE_LANGUAGE} --label ${CHAINCODE_LABEL}
--- bank_chaincode/shell_scripts_lifecycle/2_install.sh
+++ bank_chaincode/shell_scripts_lifecycle/2_install.sh
@@ -10,10 +10,10 @@ CHAINCODE_LABEL="${CHAINCODE_NAME}_${CHAINCODE_VERSION}"
 CHAINCODE_SRC_CODE_PATH="/root/CLI/chaincodes/fabric_test_chaincodes/bank_chaincode/node"
 CHANCODE_LANGUAGE="node"
 
-PEER_HOST=peer2
-CORE_PEER_ADDRESS=${PEER_HOST}:7051
-CORE_PEER_MSPCONFIGPATH=/root/CLI/${ORGCA_HOST}/${ADMIN_USER}/msp
-CORE_PEER_TLS_ROOTCERT_FILE=/root/CLI/${ORGCA_HOST}/${PEER_HOST}/msp/tls/ca.crt
+export PEER_HOST=peer2
+export CORE_PEER_ADDRESS=${PEER_HOST}:7051
+export CORE_PEER_MSPCONFIGPATH=/root/CLI/${ORGCA_HOST}/${ADMIN_USER}/msp
+export CORE_PEER_TLS_ROOTCERT_FILE=/root/CLI/${ORGCA_HOST}/${PEER_HOST}/msp/tls/ca.crt
 
 peer lifecycle chaincode install ${PACKAGE_NAME} &&
 peer lifecycle chaincode queryinstalled --output json
